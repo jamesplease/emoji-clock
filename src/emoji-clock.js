f814aa80248350016ff319581be84933fe058691
Jmeas <jellyes2@gmail.com>
2015-12-20 00:31:17 -0500

    Begin work on reusing the same image
    This commit also removes the code that enabled developers to resize the image
    by styling the emoji-clock element. That idea no longer works with the sprite!

--- src/emoji-clock.js
+++ src/emoji-clock.js
@@ -23,41 +23,21 @@ function roundTime(hour, minute) {
 }
 
 // Pass in the `timeValue`, get back an image element for that emoji clock
-function generateImageElement(timeValue) {
+function generateClassName(timeValue) {
   // The element only supports times in the format hours:minutes;
-  // leading zeros (like 03:20) are okay.
-  var timeValues = timeValue.split(':');
-  var hour = Number(timeValues[0]);
-  var minute = Number(timeValues[1]);
+    // leading zeros (like 03:20) are okay.
+    var timeValues = timeValue.split(':');
+    var hour = Number(timeValues[0]);
+    var minute = Number(timeValues[1]);
 
-  var roundedTimeValues = roundTime(hour, minute);
-  var roundedHour = String(roundedTimeValues.hour);
-  var roundedMinute = String(roundedTimeValues.minute);
-  var emojiTimeValue = roundedHour;
-  if (roundedMinute !== '0') {
-    emojiTimeValue += roundedMinute;
-  }
-
-  // The wrapper is ultimately because I want to use sprites with the `img` tag.
-  // For that to work, the CSS `clip` property must be used on the `img`. That
-  // property requires that the image itself be absolutely positioned. This
-  // relatively positioned parent ensures that the child isn't positioned
-  // in relationship to anything outside of the web component.
-  var wrapper = document.createElement('div');
-  wrapper.style.position = 'relative';
-
-  var img = document.createElement('img');
-  img.alt = hour + ':' + minute;
-  img.src = '../dist/emoji-clock.png';
-  // This allows developers to adjust the dimensions of this image
-  // by applying the changes to the `emoji-clock` element directly.
-  img.style.width = 'inherit';
-  img.style.height = 'inherit';
-  img.style.position = 'absolute';
-  img.className = 'icon-clock' + emojiTimeValue;
-
-  wrapper.appendChild(img);
-  return wrapper;
+    var roundedTimeValues = roundTime(hour, minute);
+    var roundedHour = String(roundedTimeValues.hour);
+    var roundedMinute = String(roundedTimeValues.minute);
+    var emojiTimeValue = roundedHour;
+    if (roundedMinute !== '0') {
+      emojiTimeValue += roundedMinute;
+    }
+    return `icon-clock${emojiTimeValue}`;
 }
 
 var emojiClock = Object.create(HTMLElement.prototype);
@@ -66,9 +46,32 @@ Object.assign(emojiClock, {
   createdCallback: function () {
     var shadow = this.createShadowRoot();
     var timeValue = this.getAttribute('time');
-    var img = generateImageElement(timeValue);
+    var className = generateClassName(timeValue);
+
+    // I know, I know. This is a little repetitious based on the code in
+    // `generateClassName`. I couldn't think of a way to rewrite this code
+    // any DRYer without having a silly function.
+    var timeValues = timeValue.split(':');
+    var hour = Number(timeValues[0]);
+    var minute = Number(timeValues[1]);
+
+    // The wrapper is ultimately because I want to use sprites with the `img` tag.
+    // For that to work, the CSS `clip` property must be used on the `img`. That
+    // property requires that the image itself be absolutely positioned. This
+    // relatively positioned parent ensures that the child isn't positioned
+    // in relationship to anything outside of the web component.
+    var wrapper = document.createElement('div');
+    wrapper.style.position = 'relative';
+
+    var img = document.createElement('img');
+    img.src = '../dist/emoji-clock.png';
+    img.alt = `${hour}:${minute}`;
+    img.style.position = 'absolute';
+    img.className = className;
+
+    wrapper.appendChild(img);
     shadow.innerHTML += '<style>@import url("../dist/emoji-clock.css");</style>';
-    shadow.appendChild(img);
+    shadow.appendChild(wrapper);
   },
 
   attributeChangedCallback: function (attr, oldVal, newVal) {
